349786a1a5c689e2cdb6b0b06f1d6f0bc658b6e2
asaadmartinn <83102496+asaadmartinn@users.noreply.github.com>
2022-12-15 21:48:32 -0500

    Add files via upload

---
+++ Assignment 2/boggle_solver.js
@@ -0,0 +1,136 @@
+/**
+ * Given a Boggle board and a dictionary, returns a list of available words in
+ * the dictionary present inside of the Boggle board.
+ * @param {string[][]} grid - The Boggle game board.
+ * @param {string[]} dictionary - The list of available words.
+ * @returns {string[]} solutions - Possible solutions to the Boggle board.
+ */
+//worked with deante
+
+
+exports.findAllSolutions = function (grid, dictionary) {
+ let solutions = [];
+ 
+  if (grid == null || dictionary == null){
+  return solutions;}
+ 
+  let N = grid.length;
+  for (let i = 0; i < N; i++) {
+    if (grid[i].length != N) {
+      return solutions;
+    }
+  }
+  lowercaseFunctionHelp(grid, dictionary);
+ 
+  let trie = newHash(dictionary);
+  let solutionSet = new Set();
+ 
+  for (let y = 0; y < N; y++) {
+    for (x = 0; x < N; x++) {
+      let word = "";
+      let visited = new Array(N).fill(false).map(() => new Array(N).fill(false));
+      findWords(word, y, x, grid, visited, trie, solutionSet);
+    }
+  }
+ 
+  solutions = Array.from(solutionSet);
+  return solutions;
+};
+ 
+lowercaseFunctionHelp = function (grid, dict) {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      grid[i][j] = grid[i][j].toLowerCase();
+    }
+  }
+ 
+  for (let i = 0; i < dict.length; i++) {
+    dict[i] = dict[i].toLowerCase();
+  }
+};
+ 
+findWords = function (word, y, x, grid, visited, trie, solutionSet) {
+  let adjMatrix = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [0, -1],
+  ];
+ 
+  if (
+    y < 0 ||
+    x < 0 ||
+    y >= grid.length ||
+    x >= grid.length ||
+    visited[y][x] == true
+  )
+    return;
+ 
+  word += grid[y][x];
+ 
+  if (isPrefix(word, trie)) {
+    visited[y][x] = true;
+ 
+    if (isWord(word, trie)) {
+      if (word.length >= 3) solutionSet.add(word);
+    }
+ 
+    for (let i = 0; i < 8; i++) {
+      findWords(
+        word,
+        y + adjMatrix[i][0],
+        x + adjMatrix[i][1],
+        grid,
+        visited,
+        trie,
+        solutionSet
+      );
+    }
+  }
+  visited[y][x] = false;
+};
+ 
+isPrefix = function (word, trie) {
+  return trie[word] != undefined;
+};
+ 
+isWord = function (word, trie) {
+  return trie[word] == 1;
+};
+ 
+newHash = function (dictionary) {
+  var dict = {};
+  for (let i = 0; i < dictionary.length; i++) {
+    dict[dictionary[i]] = 1;
+    let wordlength = dictionary[i].length;
+    var str = dictionary[i];
+ 
+    for (let j = wordlength; wordlength > 1; wordlength--) {
+      str = str.substr(0, wordlength - 1);
+ 
+      if (str in dict) {
+        if (str == 1) {
+          dict[str] = 1;
+        }
+      } else {
+        dict[str] = 0;
+      }
+    }
+  }
+  return dict;
+};
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
